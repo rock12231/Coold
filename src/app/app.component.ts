48e7455c6736b2f6a1ee56b59269f865f6dee2d7
anonyms developer <developer.idblfs@gmail.com>
2021-05-09 17:55:35 +0530

    added shubham  static page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'coold';
+export class AppComponent implements OnInit {
+  self: any = true;
+
+  constructor(private meta: Meta, private title: Title) {
+  }
+  ngOnInit() {
+    const domain = window.location.hostname;
+    if (domain.indexOf('.') > 0) {
+      if (domain.split('.')[0] === 'www' || domain.split('.')[0] === 'coold') {
+        this.self = true
+      } else if (domain.split('.')[0] === 'shubham') {
+        this.self = false
+      }
+    } else {
+      this.self = true
+    }
+    console.log(domain, this.self)
+
+  }
 }
