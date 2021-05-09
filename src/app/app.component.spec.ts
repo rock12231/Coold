59eee6108c3a5dbc2d81bf5e8d4000e993ec0371
anonyms developer <developer.idblfs@gmail.com>
2021-05-09 16:24:39 +0530

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coold2'`, () => {
+  it(`should have as title 'coold'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coold2');
+    expect(app.title).toEqual('coold');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('coold2 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('coold app is running!');
   });
 });
